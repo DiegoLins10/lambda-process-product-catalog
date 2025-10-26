342ba645be1e11a9dd3728b56aa9679830f5f26e
DiegoLins10 <diegofernandeslins@gmail.com>
2025-10-26 00:56:27 -0300

    Criando integração com dynamodb

---
+++ app/src/ProductCatalogLambda/ProductCatalogLambda/Aws/DynamoService.cs
@@ -0,0 +1,38 @@
+﻿using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
+using ProductCatalogLambda.Interfaces;
+using ProductCatalogLambda.Models;
+using System.Globalization;
+
+namespace ProductCatalogLambda.Aws;
+
+public class DynamoService : IDynamoService
+{
+    private readonly IAmazonDynamoDB _dynamoClient;
+    private readonly string _tableName = "ProductCatalog";
+
+    public DynamoService(IAmazonDynamoDB dynamoClient)
+    {
+        _dynamoClient = dynamoClient;
+    }
+
+    public async Task UpsertProductAsync(Product product)
+    {
+        var item = new Dictionary<string, AttributeValue>
+        {
+            ["SKU"] = new AttributeValue { S = product.SKU },
+            ["Name"] = new AttributeValue { S = product.Name },
+            ["Description"] = new AttributeValue { S = product.Description },
+            ["Price"] = new AttributeValue { N = product.Price.ToString("F2", CultureInfo.InvariantCulture) },
+            ["Category"] = new AttributeValue { S = product.Category }
+        };
+
+        var request = new PutItemRequest
+        {
+            TableName = _tableName,
+            Item = item
+        };
+
+        await _dynamoClient.PutItemAsync(request);
+    }
+}
