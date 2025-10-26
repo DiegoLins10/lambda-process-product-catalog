414fc18832bde4d8ea863c7a0a2c6630603229ed
DiegoLins10 <diegofernandeslins@gmail.com>
2025-10-26 00:59:37 -0300

    Entidade product dynamo

---
+++ app/src/ProductCatalogLambda/ProductCatalogLambda/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductCatalogLambda.Models;
+
+public class Product
+{
+    public string SKU { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public string Category { get; set; } = string.Empty;
+}
