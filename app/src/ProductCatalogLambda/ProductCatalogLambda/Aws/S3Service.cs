582bcc090299f360a82d39d2bcc34044dd20e946
DiegoLins10 <diegofernandeslins@gmail.com>
2025-10-25 22:08:45 -0300

    Criando s3 service

---
+++ app/src/ProductCatalogLambda/ProductCatalogLambda/Aws/S3Service.cs
@@ -0,0 +1,23 @@
+﻿using Amazon.S3;
+using ProductCatalogLambda.Interfaces;
+
+namespace ProductCatalogLambda.Aws;
+
+public class S3Service : IS3Service
+{
+    private readonly IAmazonS3 _s3Client;
+
+    public S3Service(IAmazonS3 s3Client)
+    {
+        _s3Client = s3Client;
+    }
+
+    public async Task<Stream> GetObjectAsync(string bucketName, string key)
+    {
+        var response = await _s3Client.GetObjectAsync(bucketName, key);
+        var memoryStream = new MemoryStream();
+        await response.ResponseStream.CopyToAsync(memoryStream);
+        memoryStream.Position = 0;
+        return memoryStream;
+    }
+}
