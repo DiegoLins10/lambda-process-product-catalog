ef9bdd57ad5792bcd265630109b8dcb2d33699f3
DiegoLins10 <diegofernandeslins@gmail.com>
2025-12-01 01:04:36 -0300

    IAC em tf para a lambda

---
+++ infra/main.tf
@@ -0,0 +1,76 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+# ----------------------------
+#  ZIP da Lambda (.NET 8)
+# ----------------------------
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_dir  = "${path.module}/../app/src/ProductCatalogLambda/bin/Release/net8.0"
+  output_path = "${path.module}/lambda.zip"
+}
+
+# ----------------------------
+# IAM Role da Lambda
+# ----------------------------
+resource "aws_iam_role" "lambda_role" {
+  name = "${var.project}-lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+# Permissões básicas de logs
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+# ----------------------------
+# CloudWatch Log Group
+# ----------------------------
+resource "aws_cloudwatch_log_group" "lambda_lg" {
+  name              = "/aws/lambda/${var.lambda_name}"
+  retention_in_days = 30
+}
+
+# ----------------------------
+# Lambda
+# ----------------------------
+resource "aws_lambda_function" "lambda" {
+  function_name = var.lambda_name
+  role          = aws_iam_role.lambda_role.arn
+
+  handler = "ProductCatalogLambda::ProductCatalogLambda.Function::FunctionHandler"
+  runtime = "dotnet8"
+
+  filename         = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+
+  timeout = 30
+  memory_size = 1024
+
+  environment {
+    variables = var.env
+  }
+}
---
+++ infra/variables.tf
@@ -0,0 +1,25 @@
+variable "region" {
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "lambda_name" {
+  type        = string
+  default     = "product-catalog-lambda"
+}
+
+variable "project" {
+  type        = string
+  default     = "product-catalog"
+}
+
+variable "env" {
+  type        = map(string)
+  description = "Variáveis de ambiente da Lambda"
+  default     = {}
+}
+
+variable "allow_invoke_from_apigw" {
+  type    = bool
+  default = false
+}
