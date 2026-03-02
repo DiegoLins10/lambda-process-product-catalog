a99b2dd85a7db105776bdcf7d4eb26410aaa557e
DiegoLins10 <diegofernandeslins@gmail.com>
2026-03-02 01:39:34 -0300

    refactor: separar parse de evento e padronizar respostas da lambda

--- app/src/ProductCatalogLambda/Models/ProcessResponse.cs
+++ app/src/ProductCatalogLambda/Models/ProcessResponse.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace ProductCatalogLambda.Models;
 
-namespace ProductCatalogLambda.Models
+public class ProcessResponse
 {
-    public class ProcessResponse
-    {
-        public int StatusCode { get; set; }
-        public string? Status { get; set; }
-        public string? Message { get; set; }
-        public bool Sucess { get; set; }
-
-    }
+    public int StatusCode { get; set; }
+    public string? Status { get; set; }
+    public string? Message { get; set; }
+    public bool Sucess { get; set; }
 }
